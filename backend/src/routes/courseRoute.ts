7d105be5126a9d8920d63f836c24ebb0d7cbe676
dltmthilina <dltmthilinamaduranga@gmail.com>
2025-02-20 12:17:55 +0530

    add role base authorization for course routes

--- backend/src/routes/courseRoute.ts
+++ backend/src/routes/courseRoute.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { check } from "express-validator";
 import courseController from "../controllers/courseController";
+import authenticate from "../middlewares/authenticate";
+import authorize from "../middlewares/authorize";
 
 const router = express.Router();
 
@@ -11,11 +13,33 @@ router.post(
     check("description").isString().not().isEmpty(),
     check("duration").isString().not().isEmpty(),
   ],
+  authenticate,
+  authorize(["admin"]),
   courseController.createCourse
 );
-router.put("/update", courseController.updateCourse);
-router.get("/get:adminId", courseController.getCoursesByAdminId);
-router.get("/get:id", courseController.getCourseById);
-router.delete("/delete:id", courseController.deleteCourse);
+router.put(
+  "/update",
+  authenticate,
+  authorize(["admin"]),
+  courseController.updateCourse
+);
+router.get(
+  "/get:adminId",
+  authenticate,
+  authorize(["admin"]),
+  courseController.getCoursesByAdminId
+);
+router.get(
+  "/get:id",
+  authenticate,
+  authorize(["admin", "student"]),
+  courseController.getCourseById
+);
+router.delete(
+  "/delete:id",
+  authenticate,
+  authorize(["admin"]),
+  courseController.deleteCourse
+);
 
 export default router;
